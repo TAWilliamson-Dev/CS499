ebe2ac20056230028933ed92ba801c812867301f
TAWilliamson-Dev <travis.williamson@snhu.edu>
2025-09-05 01:39:52 -0500

    Initial commit of blank c++ project

---
+++ Source/GAM312_Williamson/GAM312_Williamson.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GAM312_Williamson.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GAM312_Williamson, "GAM312_Williamson" );
